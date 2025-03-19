a963bc5caee53bda1901f2e8a7030ffcd8dbc2f4
OUMAIMA-AIT-SAID <oumaimaaitsaid.dev@gmail.com>
2025-03-19 10:09:31 +0000

    ajouter methode index in postController

--- wworkshop/app/Http/Controllers/PostController.php
+++ wworkshop/app/Http/Controllers/PostController.php
@@ -7,12 +7,10 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    
     public function index()
     {
-        //
+       return Post::all();
     }
 
     /**
